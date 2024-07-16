6d966b854a616d108145354c3805e0ea0589de83
John Gammon <john_gammon@discovery.com>
2024-07-16 11:29:21 -0400

    First attempt at Day 18.

--- src/com/advent2023/Main.java
+++ src/com/advent2023/Main.java
@@ -1,22 +1,24 @@
 package com.advent2023;
 
 
-import com.advent2023.days.*;
+import com.advent2023.days.Day18;
 
 import java.util.List;
 
 public class Main {
 
-    private static final boolean IS_DEMO = true;
-    private static final List<Integer> PARTS = List.of(1);
+    private static final List<Boolean> IS_DEMO = List.of(true,false);
+    private static final List<Integer> PARTS = List.of(1,2);
 
     public static void main(String[] args) {
 
-        PARTS.forEach(part -> {
-            String message = "Advent 2023 " + (IS_DEMO ? "Demo " : "") + "- Part " + part;
+        IS_DEMO.forEach(isDemo -> PARTS.forEach(part -> {
+
+            String message = "Advent 2023 " + (isDemo ? "Demo " : "") + "- Part " + part;
             System.out.println("\n\033[1m\033[4m" + message + "\033[0m");
 
-            Day18.main(part, IS_DEMO);
-        });
+            Day18.main(part, isDemo);
+
+        }));
     }
 }
